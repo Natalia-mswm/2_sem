27b5e842d011a247a2a561fc46c9b81dce86e7f7
Natalia-mswm <149807177+Natalia-mswm@users.noreply.github.com>
2024-03-20 11:04:21 +0500

    Update test_ml_HW2_Alex_R.py

--- test_ml_HW2_Alex_R.py
+++ test_ml_HW2_Alex_R.py
@@ -1,1 +1,8 @@
+from streamlit.testing.v1 import AppTest
 
+at = AppTest.from_file("ml_HW2_Alex_R.py")
+
+
+def test_load_image():
+    at = AppTest.from_file("ml_HW2_Alex_R.py")
+    at.run(timeout=10) 
